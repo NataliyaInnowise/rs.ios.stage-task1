6ab07644018b7ce044eb78f803e7c9debff74848
Natalia Lazovskaya <natalia.lazovskaya@innowise.com>
2024-09-06 18:49:21 +0200

    add numberConverter task solution

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,21 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
-}
+    
+    if (number == nil) {
+        return @[];
+    }
 
+    NSString *string = [number stringValue];
+    string = [string stringByReplacingOccurrencesOfString:@"-" withString:@""];
+    
+    NSMutableArray *arrayToReturn = [NSMutableArray new];
+    
+    for (NSInteger index = string.length - 1; index >= 0; index--) {
+        UniChar character = [string characterAtIndex:index];
+        NSString *charToAdd = [NSString stringWithFormat: @"%c", character];
+        [arrayToReturn addObject:charToAdd];
+    }
+    return arrayToReturn;
+}
 @end
