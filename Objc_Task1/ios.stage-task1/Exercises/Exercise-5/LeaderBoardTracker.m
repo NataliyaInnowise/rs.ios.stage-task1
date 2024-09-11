4c5548f3b16ffaab1c4801438576c0ae64c7529f
Natalia Lazovskaya <natalia.lazovskaya@innowise.com>
2024-09-11 16:25:11 +0200

    add 5 task solution

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,32 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    
+    NSOrderedSet *orderedSet = [NSOrderedSet orderedSetWithArray: rankedArray];
+    NSArray <NSNumber *> *uniqueArray = [orderedSet array];
+    
+    NSMutableArray <NSNumber *> *resultArray = [NSMutableArray new];
+    
+    
+    for (NSNumber *playerScore in playerArray) {
+        NSInteger rank = [self calculateRankForScore:playerScore inRankedArray:uniqueArray];
+        [resultArray addObject:@(rank)];
+    }
+    
+    return resultArray;
+}
+
+- (NSInteger)calculateRankForScore:(NSNumber *)playerScore inRankedArray:(NSArray<NSNumber *> *)rankedArray {
+    
+    NSInteger rank = 1;
+    for (NSNumber *rankedScore in rankedArray) {
+        if ([playerScore intValue] >= [rankedScore intValue]) {
+            return rank;
+        } else {
+            rank++;
+        }
+    }
+    return rank;
 }
 
 @end
